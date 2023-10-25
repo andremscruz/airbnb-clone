8eb968101f01985bdfe3b8e239ca67b1e3e298c9
Andre Cruz <71467368+andremscruz@users.noreply.github.com>
2023-10-25 12:03:28 +0100

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -22,7 +22,7 @@ function Card(props) {
                 <img alt = "star" src={star} className="card--star" />
                 <span>{props.card.stats.rating}</span>
                 <span className="gray">({props.card.stats.reviewCount}) • </span>
-                <span className="gray">{props.card.location}</span>
+                <span className="gray" style={{marginLeft: "3px"}}>{props.card.location}</span>
             </div>
             <p className="card--title">{props.card.title}</p>
             <p className="card--price"><span className="bold">From ${props.card.price}</span> / person</p>
